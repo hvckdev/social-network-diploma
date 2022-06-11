fd8296e8d3d15138d8e0a8d22d4b38bfb90b363a
hvckxm <barsiklegend@gmail.com>
2022-06-11 11:17:49 +0400

    New homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,41 @@
                 @auth
                     Hello, {{ auth()->user()->name }}!
                 @else
-                    Hello! You are not authorized.
+                    <div class="row">
+                        <div class="col">
+                            Hello! You are not authorized.
+                        </div>
+                        <div class="col">
+                            Register to be able to:
+                        </div>
+                    </div>
                 @endif
             </div>
             <div class="card-body">
                 @auth
                     <a class="btn btn-primary mt-5" href="{{ route('dashboard') }}">Go To Dashboard</a>
                 @else
-                    <p>If you dont have an account you have to register it
-                        <a href="{{ route('register') }}">
-                            Here
-                        </a>.
-                    </p>
-                    <p>If you already registered, please
-                        <a href="{{ route('login') }}">Login</a>
-                    </p>
+                    <div class="row">
+                        <div class="col">
+                            <p>If you dont have an account you have to register it
+                                <a href="{{ route('register') }}">
+                                    Here
+                                </a>.
+                            </p>
+                            <p>If you already registered, please
+                                <a href="{{ route('login') }}">Login</a>
+                            </p>
+                        </div>
+                        <div class="col">
+                            <ul>
+                                <li>messaging</li>
+                                <li>add friends</li>
+                                <li>show blogs</li>
+                                <li>create own articles in blog</li>
+                                <li>show your schedule</li>
+                            </ul>
+                        </div>
+                    </div>
                 @endif
             </div>
         </div>
